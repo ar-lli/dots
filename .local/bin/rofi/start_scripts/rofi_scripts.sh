a5c4225c1d6b4fe102f99bf380f319cbf865769d
ar-lli <aricare1729@gmail.com>
2021-05-31 17:25:42 +0200

    some updates

--- .local/bin/rofi/start_scripts/rofi_scripts.sh
+++ .local/bin/rofi/start_scripts/rofi_scripts.sh
@@ -2,13 +2,31 @@
 ListCommand=$(cat $HOME/.rofi_scripts.txt | rofi -dmenu -p "scripts")
 case $ListCommand in
 
-	add_word)
-		Command="rofi_$ListCommand.sh"
-		bash $Command
+	add_word*)
+		Command=$(echo $ListCommand | sed 's/ .*//')
+		ToAdd=$(echo $ListCommand | sed "s/$Command //")
+		echo $Command
+		echo $ToAdd
+		if [[ "$ToAdd" == "$Command" ]]
+		then
+			Command="rofi_$Command.sh"
+			bash $Command
+		else
+			echo "$ToAdd" >> .words.txt
+		fi
 		;;
-	clipboard)
-		Command="rofi_$ListCommand.sh"
-		bash $Command
+	clipboard*)
+		Command=$(echo $ListCommand | sed 's/ .*//')
+		ToCopy=$(echo $ListCommand | sed "s/$Command //")
+		echo $Command
+		echo $ToCopy
+		if [[ "$ToCopy" == "$Command" ]]
+		then
+			Command="rofi_$Command.sh"
+			bash $Command
+		else
+			echo "$ToCopy" >> .clipboard.txt
+		fi
 		;;
 
 	download_tidy*)
